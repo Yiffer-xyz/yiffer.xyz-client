fbc56e92d6846db7cb09462a453463e2682cd916
ragnarob <malann.sjakk@gmail.com>
2018-10-25 14:58:16 +0200

    Finish voting modal

--- src/components/VotingModal.vue
+++ src/components/VotingModal.vue
@@ -10,6 +10,10 @@
 					<td 
 						v-for="i in 10"
 						v-bind:key="i"
+						v-bind:class="[
+							'vote-box-colored-' + currentMouseoverNumber,
+							{'vote-box-uncolored': currentMouseoverNumber>i}
+						]"
 						@mouseover="onNumberMouseover(i)"
 						@mouseout="onNumberMouseOut()"
 						@click="onNumberClick(i)"
@@ -18,6 +22,8 @@
 					</td>
 				</tr>
 			</table>
+
+			<button @click="onNumberClick(0)">Delete vote</button>
 		</div>
 	</div>
 </template>
@@ -31,7 +37,7 @@ export default {
 	},
 	data: function () {
 		return {
-			currentMouseoverNumber: this.comic.yourRating
+			currentMouseoverNumber: this.comic.yourRating,
 		}
 	},
 	methods: {
@@ -41,10 +47,30 @@ export default {
 		onNumberMouseOut () {
 			this.currentMouseoverNumber = this.comic.yourRating
 		},
-		onNumberClick ( number ) {
-
+		async onNumberClick ( number ) {
+			this.closeModal()
+			let votingResponse = await mockSendVote(number)
+			if ( votingResponse.success ) {
+				let updatedComic = await mockGetComicDetails()
+				this.$store.commit('updateOneComicInList', updatedComic)
+				this.comic.yourRating = updatedComic.yourRating
+				this.comic.userRating = updatedComic.userRating
+			}
+		},
+		closeModal () {
+			this.$store.commit('setVotingModalVisibility', false)
 		}
 	},
+	computed: {
+
+	}
+}
+
+async function mockSendVote () {
+	return {success: true}
+}
+async function mockGetComicDetails () {
+	return {"tag": "Furry", "name": "Anarchy", "keywords": ["bird", "fox mccloud", "autofellatio", "cub", "draenei", "animal crossing", "discord"], "cat": "MF", "lastUpdateNewPageCount": 0, "updated": "2015-01-01T00:00:00.000Z", "numberOfPages": 4, "userRating": 7.4, "id": 252, "created": "2017-07-01T00:00:00.000Z", "finished": 1, "artist": "Wolfy-Nail", "yourRating": 0}
 }
 </script>
 
@@ -52,6 +78,33 @@ export default {
 <style lang="scss">
 $linkColor: #3984d4;
 $themeRed: #ec2f4b;
+
+.vote-box-colored-1 { background-color: #009FFF; }
+.vote-box-colored-2 { background-color: #1A92EB; }
+.vote-box-colored-3 { background-color: #3486D7; }
+.vote-box-colored-4 { background-color: #4E79C3; }
+.vote-box-colored-5 { background-color: #686DAF; }
+.vote-box-colored-6 { background-color: #83609B; }
+.vote-box-colored-7 { background-color: #9D5487; }
+.vote-box-colored-8 { background-color: #B74773; }
+.vote-box-colored-9 { background-color: #D13B5F; }
+.vote-box-colored-10 { background-color: #EC2F4B; }
+.vote-box-colored {
+	color: white;
+}
+.vote-box-uncolored {
+	background-color: #ccc;
+	color: #222;
+}
+
+.voting-numbers-table {
+	td {
+		&:hover {
+			cursor: pointer;
+		}
+	}
+}
+
 .modal-backdrop {
 	width: 100%;
 	height: 100%;
@@ -101,6 +154,11 @@ $themeRed: #ec2f4b;
 }
 
 .dark {
+	.vote-box-uncolored {
+		background-color: #333;
+		color: white;
+	}
+	
 	.voting-modal {
 		background-color: #222426;
 		color: white;
