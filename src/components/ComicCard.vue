fadf0cfcaade666fb519e37a7314549f4fa56967
ragnarob <ragnarob@protonmail.com>
2021-05-11 20:14:32 +0200

    Load full size thumbnail for high-res screens

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -30,7 +30,10 @@
 
           <source media="(max-width: 900px)"
                   type="image/webp"
-                  :srcset="`${config.comicsBucketName}/${comicNameUrlParsed}/thumbnail-small.webp`"
+                  :srcset="`
+                    ${config.comicsBucketName}/${comicNameUrlParsed}/thumbnail-small.webp 1x,
+                    ${config.comicsBucketName}/${comicNameUrlParsed}/thumbnail.webp 2x,
+                  `"
                   @load="onLoadFinished">
 
           <img :src="`${config.comicsBucketName}/${comicNameUrlParsed}/thumbnail.jpg`"
